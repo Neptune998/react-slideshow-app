233bbf6e1834eb93defe73c78234129b704450c5
Admin <admin.push@hackerrank.workspace>
2023-08-28 17:29:38 +0000

    Auto save changes before Switch to Local IDE
    Commit by HackerRank

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Slides({slides}) {
+function Slides({ slides }) {
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+    const goToSlide = (index) => {
+        setCurrentSlideIndex(index);
+    };
+
+    const goToNextSlide = () => {
+        if (currentSlideIndex < slides.length - 1) {
+            setCurrentSlideIndex(currentSlideIndex + 1);
+        }
+    };
+
+    const goToPrevSlide = () => {
+        if (currentSlideIndex > 0) {
+            setCurrentSlideIndex(currentSlideIndex - 1);
+        }
+    };
+
+    const restartSlides = () => {
+        setCurrentSlideIndex(0);
+    };
+
+    const { title, text } = slides[currentSlideIndex];
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button
+                    data-testid="button-restart"
+                    className="small outlined"
+                    onClick={restartSlides}
+                    disabled={currentSlideIndex === 0}
+                >
+                    Restart
+                </button>
+                <button
+                    data-testid="button-prev"
+                    className="small"
+                    onClick={goToPrevSlide}
+                    disabled={currentSlideIndex === 0}
+                >
+                    Prev
+                </button>
+                <button
+                    data-testid="button-next"
+                    className="small"
+                    onClick={goToNextSlide}
+                    disabled={currentSlideIndex === slides.length - 1}
+                >
+                    Next
+                </button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{title}</h1>
+                <p data-testid="text">{text}</p>
             </div>
         </div>
     );
-
 }
 
 export default Slides;
